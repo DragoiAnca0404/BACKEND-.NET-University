61cdca60e8424d233eb75dd2e1ca2b5682e64df2
Adrian55Stack <68397161+Adrian55Stack@users.noreply.github.com>
2021-10-08 21:54:54 +0300

    Move ifs to create early return pattern

--- App_Facultate/Controller/AuthenticationController.cs
+++ App_Facultate/Controller/AuthenticationController.cs
@@ -20,14 +20,6 @@ namespace Authentication.Controller
         [HttpGet("login/")]
         public IActionResult checkLogin(String username, [FromBody] String password)
         {            
-            var userExists = _context.Utilizatori
-                .Where(s => s.username.Equals(username) && s.parola.Equals(password))
-                .Select(s => new
-            {
-                parola = s.parola,
-                username = s.prenume
-            }).Count();
-
             var userId = _context.Utilizatori
                 .Where(s => s.username.Equals(username) && s.parola.Equals(password))
                 .Select(s => new
@@ -35,39 +27,47 @@ namespace Authentication.Controller
                     id_utilizator = s.id_utilizator
                 });
 
+            if (userId == null)
+            {
+                return NotFound();
+            }
+
             var studentId = _context.Studenti
                 .Select(s => new
                 {
                     id_utilizatori = s.id_utilizatori
                 });
 
+            if (userId == studentId)
+            {
+                return Ok("Student");
+            }
+
             var adminId = _context.Administratori
                 .Select(s => new
                 {
                     id_utilizator = s.id_utilizator
                 });
+
+            if (userId == adminId)
+            {
+                return Ok("Administrator");
+
+            }
+
             var profesorId = _context.Profesori
                 .Select(s => new
                 {
                     id_utilizator = s.id_utilizator
                 });
             
-            if (userId == studentId)
-            {
-            }
-             else if (userId == adminId)
-            {
 
-            }
-             else if (userId == profesorId)
+            if (userId == profesorId)
             {
+                return Ok("Profesor");
+            }
 
-            }
-             else
-            {
-                return NotFound();
-            }
-            return userExists.Equals(1) ? Ok("User-ul exista!") : NotFound();
+            return NotFound();
         }
     }
 }
