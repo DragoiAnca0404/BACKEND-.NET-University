a0144a54aaa05d2ebbd61f48935f55cab7b89709
Dragoi Anca-Florentina <67691293+DragoiAnca0404@users.noreply.github.com>
2022-05-28 12:01:13 +0300

    Create GradesDisplayController.cs

---
+++ App_Facultate/Controller/GradesDisplayController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Data;
+using Models;
+
+namespace App_Facultate.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GradesDisplayController : ControllerBase
+    {
+        private readonly FacultateContext _context;
+
+        public GradesDisplayController(FacultateContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/GradesDisplay
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Materii>>> GetMaterii(string denumire_materie)
+        {
+            return await _context.Materii.ToListAsync();
+        }
+
+      
+    }
+}
