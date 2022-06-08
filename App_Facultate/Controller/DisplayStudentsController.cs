8a4a49d3c29cbe256bce229bed006c6e1b64cbb1
Dragoi Anca-Florentina <67691293+DragoiAnca0404@users.noreply.github.com>
2022-06-08 16:56:30 +0300

    Create DisplayStudentsController.cs

---
+++ App_Facultate/Controller/DisplayStudentsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Data;
+using Models;
+
+namespace App_Facultate.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DisplayStudentsController : ControllerBase
+    {
+        private readonly FacultateContext _context;
+
+        public DisplayStudentsController(FacultateContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/DisplayStudents
+     
+
+        // GET: api/DisplayStudents
+        [HttpGet]
+        public async Task<ActionResult<Materii>> GetMaterii(string denumire_materie)
+        {
+            var students = _context.Materii.Where(w => w.denumire_materie.Equals(denumire_materie))
+                           .Join(_context.Studenti,
+                           x => x.id_student,
+                           y => y.id_student,
+                            (x, y) => new { x, y }).Join(_context.Utilizatori,
+                           a => a.y.id_utilizator,
+                           b => b.id_utilizator, (a, b) => new { a, b }).Select(z => new {  name = z.b.nume, surname = z.b.prenume, id_user = z.a.y.id_utilizator }).ToList();
+
+            return Ok(students);
+        }
+
+    }
+}
